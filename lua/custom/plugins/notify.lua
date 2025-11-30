9bb85b51efc58db6d775051884f9d415f86e0345
Ethan Walker <ethanwalker23081@gmail.com>
2025-11-29 20:28:02 -0500

    move notify to noice configuration

--- lua/custom/plugins/notify.lua
+++
@@ -1,9 +0,0 @@
--- Config for the nvim-notify plugin to get better looking notifications
-return {
-	"rcarriga/nvim-notify",
-	lazy = false,
-	config = function()
-		require("notify").setup()
-		vim.notify = require("notify")
-	end,
-}
