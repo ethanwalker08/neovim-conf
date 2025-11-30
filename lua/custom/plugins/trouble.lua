59a9c2b25a037cfc5444bfb1f5bb1399df8f902e
Ethan Walker <ethanwalker23081@gmail.com>
2025-11-29 19:27:23 -0500

    removed unused keybinds for trouble plugin to simplify invocation

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -4,24 +4,9 @@ return {
 	cmd = "Trouble",
 	keys = {
 		{
-			"<leader>xx",
+			"<leader>x",
 			"<cmd>Trouble diagnostics toggle<cr>",
 			desc = "Diagnostics (Trouble)",
-		},
-		{
-			"<leader>xX",
-			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-			desc = "Buffer Diagnostics (Trouble)",
-		},
-		{
-			"<leader>xL",
-			"<cmd>Trouble loclist toggle<cr>",
-			desc = "Location List (Trouble)",
-		},
-		{
-			"<leader>xQ",
-			"<cmd>Trouble qflist toggle<cr>",
-			desc = "Quickfix List (Trouble)",
 		},
 	},
 }
