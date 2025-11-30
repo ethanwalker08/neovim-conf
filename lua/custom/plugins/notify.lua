7c1649c6a764ae1e6485414d5fe9198106fd407e
Ethan Walker <ethanwalker23081@gmail.com>
2025-11-29 19:29:01 -0500

    make notify plugin load on startup so it isn't lazy loaded

--- lua/custom/plugins/notify.lua
+++ lua/custom/plugins/notify.lua
@@ -1,7 +1,7 @@
-return
--- Notify for better notifications in Neovim
-{
+-- Config for the nvim-notify plugin to get better looking notifications
+return {
 	"rcarriga/nvim-notify",
+	lazy = false,
 	config = function()
 		require("notify").setup()
 		vim.notify = require("notify")
